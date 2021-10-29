7adc626827b0f7fa46548c7a8d4cfb74adf61530
Shahzaib Awan <52386022+Shahzaib0000@users.noreply.github.com>
2021-10-30 02:35:40 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {  View } from 'react-native';
+import Navigation from './src/config/Navigation'
+export default function App() {
+  return (
+    
+      <Navigation />
+
+  );
+}
+
+
